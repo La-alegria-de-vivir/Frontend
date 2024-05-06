574ab3fd2e9aeb1467916fb5e63b5ad24d387a28
amanda1686 <amanda160986@gmail.com>
2024-05-07 00:38:27 +0200

    pre navbar amd footer

--- src/Pages/Reservation/Reservation.jsx
+++ src/Pages/Reservation/Reservation.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Reservation() {
   return (
-    <div>
-      
+    <div className=' min-h-screen'>
+      <h1 className=' mt-60'>Reservation</h1>
     </div>
   )
 }
