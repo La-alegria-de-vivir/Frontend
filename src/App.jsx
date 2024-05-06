9c0702a8c16bea7ddef116e36d9a1df4a45cff02
natalia9519 <nataliayda@gmail.com>
2024-05-06 15:04:21 +0200

    structure and image

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import HomeImage from './Components/HomeImage/HomeImage.jsx'
+import Menu from './Components/Menu/Menu.jsx'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <main>
      <HomeImage/>
+     <Menu/>
     </main>
   )
 }
---
+++ src/Components/Menu/Menu.jsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react';
+
+import './Menu.css'
+
+const Menu = () => {
+  const [menu, setMenu] = useState([]);
+
+  useEffect(() => {
+    fetch('https://example.com/api/menu')
+      .then(response => response.json())
+      .then(data => setMenu(data));
+  }, []);
+
+  return (
+    <section className='section-menu'>
+      <aside>
+      <h2>Nuestra carta</h2>
+      <table>
+        <tbody>
+          {menu.map((item, index) => (
+            <tr key={index}>
+              <td>{item.title}</td>
+              <td>{item.price}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>  
+      </aside>
+      <figure>
+
+      </figure>
+      
+    </section>
+  );
+};
+
+export default Menu;
