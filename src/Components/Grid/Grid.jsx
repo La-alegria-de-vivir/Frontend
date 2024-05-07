df2dae3f1e4ce6f3bbf9498eaf2dc1967dd57d23
Mamen <fbmaricarmen@gmail.com>
2024-05-07 15:10:40 +0200

    change

--- src/Components/Grid/Grid.jsx
+++ src/Components/Grid/Grid.jsx
@@ -4,7 +4,7 @@ import Pagination from '../Pagination/Pagination';
 
 const loadImages = () => {
 
-  const imageNames = ['image1.jpg', 'image2.jpg', 'image3.jpg', 'image4.jpg', 'image5.jpg', 'image6.jpg', 'image7.jpg', 'image8.jpg'];
+  const imageNames = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg'];
 
   return imageNames.map(name => `/images/${name}`);
 }
