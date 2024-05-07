1cb299f3e8f6bdbf270103adbda3a42700ac8a0b
natalia9519 <nataliayda@gmail.com>
2024-05-07 13:21:38 +0200

    Structure inicial for aboutus with examples images

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import HomeImage from "../../Components/HomeImage/HomeImage.jsx";
 import Menu from "../../Components/Menu/Menu.jsx";
+import AboutUs from '../../Components/AboutUs/AboutUs.jsx';
 
 export default function Home() {
   return (
     <div>
     <HomeImage/>
     <Menu/>
+    <AboutUs/>
     </div>
   )
 }
