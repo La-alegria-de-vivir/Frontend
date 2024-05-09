502d785555e08c66519711c043085ca9a28216d6
natalia9519 <nataliayda@gmail.com>
2024-05-09 14:42:59 +0200

    Styles page not found

--- src/Pages/NotFound/NotFound.jsx
+++ src/Pages/NotFound/NotFound.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import notfound from '../../assets/image-n.png'
+import './NotFound.css'
+import { Link } from 'react-router-dom'
+
 export default function NotFound() {
   return (
-    <main>
-     <img src={notfound} alt="imagen" /> 
+    <main className='main-not-found'>
+      <p>Está página no está en nuestro menú</p>
+    <Link to="/"><img src={notfound} alt="imagen" /> </Link> 
     </main>
   )
 }
