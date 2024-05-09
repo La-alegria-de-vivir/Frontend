ea450e4ae4b9b858b8ace22e513932e0ae18cfea
Mamen <fbmaricarmen@gmail.com>
2024-05-09 15:37:41 +0200

    fix:botton

--- src/Components/Pagination/Pagination.jsx
+++ src/Components/Pagination/Pagination.jsx
@@ -1,29 +1,29 @@
 import React from 'react';
 
 const Pagination = ({ itemsPerPage, totalItems, paginate, currentPage }) => {
-  const pageNumbers = [];
-
-  for (let i = 1; i <= Math.ceil(totalItems / itemsPerPage); i++) {
-    pageNumbers.push(i);
-  }
+  const totalPages = Math.ceil(totalItems / itemsPerPage);
 
   return (
     <nav>
       <ul className="flex justify-center space-x-2 mt-4">
-        {pageNumbers.map(number => (
-          <li key={number} className="list-none">
-            <button
-              onClick={() => paginate(number)}
-              className={`px-4 py-2 text-white font-bold rounded transition-colors duration-300 transform ${
-                currentPage === number
-                  ? 'bg-gradient-to-r from-[#AEAF50] to-[#F3C14C]'
-                  : 'bg-gradient-to-r from-[#AEAF50] to-[#F3C14C] hover:from-[#adaf50bd] hover:to-[#F3C14C]'
-              }`}
-            >
-              {number}
-            </button>
-          </li>
-        ))}
+        <li>
+          <button
+            onClick={() => currentPage > 1 && paginate(currentPage - 1)}
+            className="px-4 py-2 text-white font-bold rounded transition-colors duration-300 transform bg-gradient-to-r from-[#AEAF50] to-[#F3C14C] hover:from-[#adaf50bd] hover:to-[#F3C14C] disabled:opacity-50"
+            disabled={currentPage === 1}
+          >
+            Anterior
+          </button>
+        </li>
+        <li>
+          <button
+            onClick={() => currentPage < totalPages && paginate(currentPage + 1)}
+            className="px-4 py-2 text-white font-bold rounded transition-colors duration-300 transform bg-gradient-to-r from-[#AEAF50] to-[#F3C14C] hover:from-[#adaf50bd] hover:to-[#F3C14C] disabled:opacity-50"
+            disabled={currentPage === totalPages}
+          >
+            Siguiente
+          </button>
+        </li>
       </ul>
     </nav>
   );
