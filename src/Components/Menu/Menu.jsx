d1a3a77f43e8e65cdcd67d224915fb7283ee2c42
natalia9519 <nataliayda@gmail.com>
2024-05-10 12:22:46 +0200

    fix: Change styles of menu with the call to the back

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -1,33 +1,46 @@
 import React, { useState, useEffect } from 'react';
-import './Menu.css'
+import './Menu.css';
 
 const Menu = () => {
   const [menu, setMenu] = useState([]);
 
   useEffect(() => {
-    fetch('https://example.com/api/menu')
+    fetch('/api/menu/getmenu')
       .then(response => response.json())
-      .then(data => setMenu(data));
+      .then(data => {
+        if (data && Array.isArray(data.menu)) {
+          setMenu(data.menu);
+        } else {
+          console.error('La respuesta de la API no contiene un array de menú válido:', data);
+        }
+      })
+      .catch(error => {
+        console.error('Error al obtener los datos del menú:', error);
+      });
   }, []);
 
   return (
     <section className='section-menu'>
-      <aside>
-      <h2>Nuestra carta</h2>
-      <table>
-        <tbody>
-          {menu.map((item, index) => (
-            <tr key={index}>
-              <td>{item.title}</td>
-              <td>{item.price}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>  
-      </aside>
-      <figure>
-      </figure>
-      
+      <h2>Nuestra <span className='title-2-word'>carta</span></h2>
+      <hr className='hr1' />
+      <hr className='hr2' />
+      <div>
+        <aside>
+          <table>
+            <tbody>
+              {menu.map((item, index) => (
+                <tr key={index}>
+                  <td>{item.title}</td>
+                  <td>{item.price}€</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </aside>
+        <figure>
+        </figure>
+        </div>
+
     </section>
   );
 };
