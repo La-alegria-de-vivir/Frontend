82e65df7cc8b6a9a1c807f5bc101876009a643ae
MarPrieto7 <mmarprietogarcia@gmail.com>
2024-05-13 15:17:59 +0200

    fix:update bookingform

--- src/Components/BookingForm/BookingForm.jsx
+++ src/Components/BookingForm/BookingForm.jsx
@@ -1,14 +1,35 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 
 const BookingForm = () => {
-    const [maxNumberOfPeople, setMaxNumberOfPeople] = useState(10); // Máximo número de comensales en sala o terraza
-    const [selectedZone, setSelectedZone] = useState('Sala');
+    const handleSubmit = async (values) => {
+        const username = localStorage.getItem('username');
+        const bookingData = {
+            people: values.numberOfPeople,
+            place: values.place,
+            date: values.date,
+            hour: values.hour,
+            name: username,
+            phoneNumber: values.phoneNumber
+        };
 
-    const handleZoneChange = (event) => {
-        setSelectedZone(event.target.value);
-        // Actualizar el número máximo de comensales según la zona seleccionada
-        setMaxNumberOfPeople(10); // Se establece un máximo de 10 personas para ambas zonas
+        try {
+            const response = await fetch('api/reserve/create', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(bookingData)
+            });
+
+            if (response.ok) {
+                console.log('¡Reserva exitosa!');
+                // Puedes realizar alguna acción después de una reserva exitosa
+            } else {
+                throw new Error('Error al crear reserva');
+            }
+        } catch (error) {
+            console.error('Error al crear reserva:', error);
+            // Puedes manejar el error de alguna manera
+        }
     };
 
     return (
@@ -17,23 +38,22 @@ const BookingForm = () => {
             <div className="flex justify-between mt-8">
                 <aside className="w-1/2 p-4 mr-10">
                     <Formik
-                        initialValues={{ numberOfPeople: 1, selectedDate: '', selectedTime: '' }}
+                        initialValues={{
+                            numberOfPeople: 1,
+                            place: 'Sala',
+                            date: '',
+                            hour: '',
+                            phoneNumber: ''
+                        }}
                         validate={(values) => {
                             const errors = {};
-                            if (values.numberOfPeople > maxNumberOfPeople) {
-                                errors.numberOfPeople = `El número máximo de comensales en ${selectedZone.toLowerCase()} es ${maxNumberOfPeople}`;
-                            }
                             if (values.numberOfPeople < 1) {
                                 errors.numberOfPeople = 'El número mínimo de comensales es 1';
                             }
                             return errors;
                         }}
                         onSubmit={(values) => {
-                            console.log('Reserva realizada:', values);
-                            // Guardar datos en Local Storage
-                            localStorage.setItem('bookingData', JSON.stringify(values));
-                            // Redirigir a otra página para finalizar la reserva
-                            window.location.href = '/finalizar-reserva';
+                            handleSubmit(values);
                         }}
                     >
                         {({ values, handleChange }) => (
@@ -44,39 +64,29 @@ const BookingForm = () => {
                                     <ErrorMessage name="numberOfPeople" component="div" className="text-red-500 mt-1" />
                                 </div>
                                 <div className="mb-4">
-                                    <label htmlFor="zone" className="block font-medium mb-1">Zona</label>
-                                    <Field as="select" id="zone" name="selectedZone" value={selectedZone} onChange={handleZoneChange} className="w-full p-2 border border-gray-300 rounded-md">
+                                    <label htmlFor="place" className="block font-medium mb-1">Lugar</label>
+                                    <Field as="select" id="place" name="place" className="w-full p-2 border border-gray-300 rounded-md">
                                         <option value="Sala">Sala</option>
                                         <option value="Terraza">Terraza</option>
                                     </Field>
                                 </div>
-                            </Form>
-                        )}
-                    </Formik>
-                </aside>
-                <article className="w-1/2 p-4 mr-10">
-                    <Formik
-                        initialValues={{ selectedDate: '', selectedTime: '' }}
-                        onSubmit={(values) => {
-                            console.log('Fecha y hora seleccionadas:', values);
-                            // Puedes realizar alguna acción con los datos de fecha y hora aquí
-                        }}
-                    >
-                        {({ values, handleChange }) => (
-                            <Form className="w-full">
                                 <div className="mb-4">
                                     <label htmlFor="date" className="block font-medium mb-1">Fecha</label>
-                                    <Field type="date" id="date" name="selectedDate" className="w-full p-2 border border-gray-300 rounded-md" />
+                                    <Field type="date" id="date" name="date" className="w-full p-2 border border-gray-300 rounded-md" />
                                 </div>
                                 <div className="mb-4">
-                                    <label htmlFor="time" className="block font-medium mb-1">Hora</label>
-                                    <Field type="time" id="time" name="selectedTime" className="w-full p-2 border border-gray-300 rounded-md" />
+                                    <label htmlFor="hour" className="block font-medium mb-1">Hora</label>
+                                    <Field type="time" id="hour" name="hour" className="w-full p-2 border border-gray-300 rounded-md" />
+                                </div>
+                                <div className="mb-4">
+                                    <label htmlFor="phoneNumber" className="block font-medium mb-1">Número de teléfono</label>
+                                    <Field type="tel" id="phoneNumber" name="phoneNumber" className="w-full p-2 border border-gray-300 rounded-md" />
                                 </div>
                                 <button type="submit" className="bg-gradient-to-r from-[#AEAF50] to-[#F3C14C] hover:from-[#adaf50bd] hover:to-[#F3C14C] text-white font-bold py-2 px-4 rounded-md">Reservar</button>
                             </Form>
                         )}
                     </Formik>
-                </article>
+                </aside>
             </div>
         </div>
     );
