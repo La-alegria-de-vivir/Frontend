03361f2904d0ec8ce44f20379bc43ed13f49ded3
amanda1686 <amanda160986@gmail.com>
2024-05-15 19:38:49 +0200

    feat:add lega lwarning, cookies and privacy policy

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import Footer from './Components/Footer/Footer.jsx';
 import Home from './Pages/Home/Home.jsx';
 import NotFound from './Pages/NotFound/NotFound.jsx'
 import ReservationPage from './Pages/ReservationPage/ReservationPage.jsx';
+import PrivacyPolicy from './Pages/PrivacyPolicy/PrivacyPolicy.jsx';
+import LegalWarning from './Pages/LegalWarning/LegalWarning.jsx';
+import CookiesPolicy from './Pages/CookiesPolicy/CookiesPolicy.jsx';
 
 function App() {
   
@@ -14,6 +17,9 @@ function App() {
       <Routes>
         <Route path='/reservation' element={<ReservationPage/>}/>
         <Route path='/' element={<Home/>}/>
+        <Route path='/privacy-policy' element={<PrivacyPolicy/>}/>
+        <Route path='/legal-warning' element={<LegalWarning/>}/>
+        <Route path='/cookies-policy' element={<CookiesPolicy/>}/>
         <Route path="*" element={<NotFound />} />
       </Routes>
 
