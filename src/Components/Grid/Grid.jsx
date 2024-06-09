26f4d7f9c68e95e6ecdd34eccd949f40d7bb8d61
Mamen <fbmaricarmen@gmail.com>
2024-06-09 15:01:59 +0200

    fit: changes in image sizes and allergens

--- src/Components/Grid/Grid.jsx
+++ src/Components/Grid/Grid.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Pagination from '../Pagination/Pagination';
-import './Grid.css';
 import Modal from '../Modal/Modal';
+import './Grid.css';
 
 const Grid = () => {
   const [images, setImages] = useState([]);
@@ -20,6 +20,7 @@ const Grid = () => {
             title: item.title,
             description: item.description,
             ingredients: item.ingredients || [],
+            allergens: item.allergens ? item.allergens.split(',') : [],  // Convertir la cadena de alérgenos en un array
             id: item._id || item.id
           }));
           setImages(processedData);
@@ -50,26 +51,30 @@ const Grid = () => {
 
   return (
     <section className="container mx-auto px-4">
-      <div className='section-AboutUs-div1 text-center'>
+      <div className='section-AboutUs-div1'>
         <h2>Nuestros <span className='title-2-word'>Platos</span></h2>
       </div>
       <hr className='hr1'/>
-      <hr className='hr2'/>
+      <hr className='hr2' style={{marginBottom:"2rem"}}/>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mt-4">
         {currentImages.map((menu) => (
+         
           <div 
-            key={menu.id} 
-            className="relative max-w-sm rounded overflow-hidden shadow-lg mx-auto cursor-pointer transform transition-transform hover:scale-105"
-            onClick={() => openModal(menu)}
-          >
-            <img src={menu.url} alt={menu.title} className="w-full opacity-90 hover:opacity-100 transition-opacity" />
-            <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center opacity-0 hover:opacity-100 transition-opacity">
-              <span className="text-white text-lg">Ver más</span>
+              key={menu.id} 
+              className="relative max-w-sm rounded overflow-hidden shadow-lg mx-auto cursor-pointer transform transition-transform hover:scale-105"
+              onClick={() => openModal(menu)}
+            >
+              <div className="image-container">
+                <img src={menu.url} alt={menu.title} className="opacity-90 hover:opacity-100 transition-opacity" />
+              </div>
+              <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center opacity-0 hover:opacity-100 transition-opacity">
+                <span className="text-white text-lg">Ver más</span>
+              </div>
+              <div className="px-6 py-4">
+                <p className="text-gray-700 text-base">{menu.title}</p>
+              </div>
             </div>
-            <div className="px-6 py-4">
-              <p className="text-gray-700 text-base">{menu.title}</p>
-            </div>
-          </div>
+
         ))}
       </div>
       <Pagination 
--- src/Components/Modal/Modal.jsx
+++ src/Components/Modal/Modal.jsx
@@ -1,24 +1,60 @@
 import React from 'react';
+import { FaFish, FaEgg, FaWineGlassAlt } from 'react-icons/fa';
+import { GiWheat, GiPeanut, GiSadCrab, GiAlmond, GiSesame, GiHerbsBundle, GiBrandyBottle, GiJellyBeans, GiShrimp } from 'react-icons/gi';
+import { LuMilk } from 'react-icons/lu';
+import { PiBeerBottleFill } from "react-icons/pi";
+
+const allergenIcons = {
+  gluten: <GiWheat />,
+  marisco: <GiShrimp />,
+  huevos: <FaEgg />,
+  pescado: <FaFish />,
+  cacahuetes: <GiPeanut />,
+  soja: <GiBrandyBottle />,
+  lácteos: <LuMilk />,
+  frutos: <GiAlmond />,
+  apio: <GiHerbsBundle />,
+  mostaza: <PiBeerBottleFill />,
+  sésamo: <GiSesame />,
+  sulfitos: <FaWineGlassAlt />,
+  altramuces: <GiJellyBeans />,
+  moluscos: <GiSadCrab />,
+  consultar: "Consultar con cocina"
+};
 
 const Modal = ({ dish, closeModal }) => {
   return (
     <div className="fixed inset-0 bg-gray-800 bg-opacity-75 flex items-center justify-center z-50">
-      <div className="bg-white rounded-lg p-6 relative max-w-sm w-full mx-4 overflow-auto max-h-screen">
+      <div className="bg-white rounded-lg p-4 relative w-full max-w-sm mx-4">
         <button
           onClick={closeModal}
-          className="absolute top-2 right-2 text-gray-700 hover:text-gray-900 text-4xl"
+          className="absolute top-2 right-2 text-gray-700 hover:text-gray-900 text-2xl"
         >
           &times;
         </button>
-        <h2 className="text-2xl font-bold mb-4">{dish.title}</h2>
-        <img src={dish.url} alt={dish.title} className="w-full h-auto mb-4 rounded" />
-        <p className="mb-4">{dish.description}</p>
-      
-        <ul className="list-disc list-inside">
+        <h2 className="text-lg font-bold mb-2 text-center">{dish.title}</h2>
+        <div className="flex justify-center mb-2">
+          <img src={dish.url} alt={dish.title} className="modal-image object-cover rounded" />
+        </div>
+        <p className="mb-2 text-sm text-center">{dish.description}</p>
+        <ul className="list-disc list-inside mb-2 text-sm">
           {dish.ingredients.map((ingredient, index) => (
             <li key={index}>{ingredient}</li>
           ))}
         </ul>
+        <div className="allergens">
+          <h3 className="text-md font-bold mb-1 text-center">Alérgenos</h3>
+          <ul className="list-none flex flex-wrap gap-2 justify-center">
+            {dish.allergens.map((allergen, index) => (
+              <li key={index} className="flex items-center gap-1 bg-gray-200 px-2 py-1 rounded">
+                {allergenIcons[allergen.trim().toLowerCase()] && (
+                  <span className="text-lg">{allergenIcons[allergen.trim().toLowerCase()]}</span>
+                )}
+                <span className="text-sm hidden">{allergen}</span>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   );
