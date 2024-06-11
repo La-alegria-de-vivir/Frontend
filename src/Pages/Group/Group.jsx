0d56cdca511f6d8388871f0f973ebd2edb57c1f1
natalia9519 <nataliayda@gmail.com>
2024-06-11 10:05:14 +0200

    fix:updating pages of group

--- src/Pages/Group/Group.jsx
+++ src/Pages/Group/Group.jsx
@@ -2,15 +2,14 @@ import React from 'react'
 
 export default function Group() {
   return (
-    <main className="flex flex-col items-center justify-start h-screen pt-32">
-      <h2 className="text-6xl">Grup<span className='title-2-word'>os</span></h2>
+    <main className="flex flex-col items-center justify-start h-screen pt-32" style={{marginBottom:'7rem'}}>
+      <h2 className="text-4xl">Estamos cocinando la pagína <span className='title-2-word'>vuelva más tarde</span></h2>
       <hr className='hr1' />
       <hr className='hr2' />
       <section className='flex flex-wrap justify-center gap-4 w-[95%] mt-8'>
-        <img className=" h-64 " src='https://firebasestorage.googleapis.com/v0/b/alegria-de-vivir-99.appspot.com/o/Menu%201.png?alt=media&token=30aca0b3-abd4-4068-807f-a7ea0159ec21' alt='Menu de grupo 1' />
-        <img className=" h-64 " src='https://firebasestorage.googleapis.com/v0/b/alegria-de-vivir-99.appspot.com/o/Menu%202.png?alt=media&token=22b024f8-3f77-4228-ab5a-0dfb269b8ffc' alt='Menu de grupo 2' />
-        <img className=" h-64 " src='https://firebasestorage.googleapis.com/v0/b/alegria-de-vivir-99.appspot.com/o/Menu%203.png?alt=media&token=5ee43a7c-4b5e-480a-a986-3bc205eeb6de' alt='Menu de grupo 3' />
+        <img className="h-[30rem] rounded-[12%] mb-[2rem] "  src='https://firebasestorage.googleapis.com/v0/b/alegria-de-vivir-99.appspot.com/o/ON_WORK.png?alt=media&token=37d016b4-e7d6-448e-a185-c2035c2dc241' alt='Chef cocinando' />
+        
       </section>
-    </main>
+    </main >
   )
 }
